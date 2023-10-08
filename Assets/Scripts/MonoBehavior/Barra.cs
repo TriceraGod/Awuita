c436098b9b6854000cf7b0b52e835d20d26698ae
Brandon.D.Vargas <brandon.vargas2906@gmail.com>
2023-10-07 21:26:01 -0600

    Creacion Scripts
    Creacion de Carpeta Scripts, subcarpeta
    MnoBehaviors.
    Script Modules.
    Script Barra

---
+++ Assets/Scripts/MonoBehavior/Barra.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Barra : MonoBehaviour
+{
+public float counter;
+public float maxCounter;
+public float startingCounter;
+public string nombre;
+
+void Progreso(){
+
+
+}
+
+}
---
+++ Assets/Scripts/MonoBehavior/Module.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Module : MonoBehaviour
+{
+public float contCALV;
+public float contTEMP;
+public float contCNTA;
+public float contNIVA;
+public string nombre;
+}
